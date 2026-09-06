af5bb3241ed9df2a2b899b411040bbc3737c3214
thenikolaika1 <thenikolaika@gmail.com>
2026-09-06 16:26:58 +0300

    Bump app cache to v31

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='calisthenics-v30';
-const STATIC=['./','./index.html','./styles.css?v=24','./contrast.css?v=24','./calendar-nav.css?v=26','./app.js?v=28','./l-sit-update.js?v=29','./band-volume.js?v=30','./tab-animation.js?v=24','./calendar-nav.js?v=28','./manifest.webmanifest?v=27','./app-icon.png?v=27'];
+const CACHE='calisthenics-v31';
+const STATIC=['./','./index.html','./styles.css?v=24','./contrast.css?v=24','./calendar-nav.css?v=26','./app.js?v=28','./l-sit-update.js?v=29','./band-volume.js?v=30','./program-update-v31.js?v=31','./tab-animation.js?v=24','./calendar-nav.js?v=28','./manifest.webmanifest?v=27','./app-icon.png?v=27'];
 const IMAGES=['./muscle-up.png','./one-arm-pull-up.png','./front-lever.png','./planche.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll([...STATIC,...IMAGES])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
